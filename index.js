aa9652d7a454f89c28595948c9d88665ad0f6526
ndxbxrme <lewis_the_cat@hotmail.com>
2021-02-19 07:03:22 +0000

    pass app to controller

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Minifrdg = () => {
+const Minifrdg = (rootSelector) => {
   let route = null;
   let params = null;
   const base = {name:''};
@@ -13,15 +13,15 @@ const Minifrdg = () => {
   const fill = (template, ctrl) => template.replace(/\{\{(.+?)\}\}/g, (all, str) => (new Function("with(this) {return " + str + "}")).call(ctrl));
   const on = (eventName, cb) => (callbacks[eventName] = callbacks[eventName] || []).push(cb);
   const fireCallbacks = (eventName, data) => (callbacks[eventName] || []).forEach(fn => fn(data));
-  const inflate = (name, data) => fill(template = templates[name] || '', (hooks[route] = data || controllers[name] && controllers[name]() || {}));
+  const inflate = (name, data) => fill(template = templates[name] || '', (hooks[app.route] = data || controllers[name] && controllers[name](app) || {}));
   const refresh = () => {
-    ['app', ...components].forEach((component) => ($(component) || {}).innerHTML = inflate(component==='app' && route || component, hooks[route]));
-    $$('a').forEach(anchor => anchor.href && !anchor.target && (anchor.onclick = () => {window.app.goto(anchor.href.replace(document.location.origin,''));return false}));
+    [rootSelector || 'app', ...components].forEach((component) => ($(component) || {}).innerHTML = inflate(component==='app' && app.route || component, hooks[app.route]));
+    $$('a').forEach(anchor => anchor.href && !anchor.target && (anchor.onclick = () => {goto(anchor.href.replace(document.location.origin,''));return false}));
   }
   const setState = () => {
     (fireCallbacks('cleanup') || (delete callbacks.cleanup)) && (hooks = {});
-    [route, ...params] = (useHash && window.location.hash.replace('#', '').replace(/^\//, '') || window.location.pathname.replace(/^\//, '')).replace(base.name, '').split(/\//g);
-    route = templates[route] && route || 'dashboard';
+    [route, ...app.params] = (useHash && window.location.hash.replace('#', '').replace(/^\//, '') || window.location.pathname.replace(/^\//, '')).replace(base.name, '').split(/\//g);
+    app.route = templates[route] && route || 'dashboard';
     refresh();
   };
   const goto = (route) => {
@@ -32,6 +32,7 @@ const Minifrdg = () => {
     }
     setState();
   };
-  return {route,params,base,useHash,templates,controllers,callbacks,hooks,components,$,$$,fill,on,fireCallbacks,setState,goto,refresh,start: () => setState(),fns:{},vars:{}};
+  const app = {route,params,base,useHash,templates,controllers,callbacks,components,$,$$,fill,on,fireCallbacks,setState,goto,refresh,start: () => setState(),fns:{},vars:{}};
+  return app;
 }
 (typeof(module)!=='undefined') && (module.exports = Minifrdg) || (window.Minifrdg = Minifrdg)
